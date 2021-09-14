4f4b9355fe5176be8e3b8f080ac222db74a930c0
dongoc <dongoc21hj@gmail.com>
2021-09-14 23:59:27 +0900

    210914_9_PalindromeNumber

--- 210914_9_PalindromeNumber.js
+++ 210914_9_PalindromeNumber.js
@@ -7,5 +7,14 @@
 
 // first try
 var isPalindrome = function(x) {
+  if (x < 0) return false
+  if (x < 10) return true
 
+  const str = x.toString()
+
+  for (let i = 0; i < Math.floor(str.length / 2); i += 1) {
+      if (str[i] !== str[str.length - 1 - i]) return false
+  }
+
+  return true
 };
