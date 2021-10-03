f4634837c83c26932334f862902fd909b8416f7a
dongoc <dongoc21hj@gmail.com>
2021-10-03 19:38:01 +0900

    211003_27_RemoveElement

---
+++ 211003_27_RemoveElement.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/remove-element/
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+
+};
