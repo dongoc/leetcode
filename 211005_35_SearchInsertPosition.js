5ec763bfd8ad90b1a740c89fb0184b667a42020c
dongoc <dongoc21hj@gmail.com>
2021-10-05 16:12:43 +0900

    211005_35_SearchInsertPosition

---
+++ 211005_35_SearchInsertPosition.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/search-insert-position/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+
+};
