fd64d6a564d07feb0b0644145916be919f27396d
dongoc <dongoc21hj@gmail.com>
2021-10-06 22:39:37 +0900

    211006_35_SearchInsertPosition

--- 211005_35_SearchInsertPosition.js
+++ 211005_35_SearchInsertPosition.js
@@ -6,5 +6,20 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
+  let first = 0
+  let last = nums.length - 1
+  let mid = Math.floor((first + last) / 2)
 
+  while(mid >= first && mid <= last) {
+    if (nums[mid] === target) return mid
+    if (nums[mid] > target) {
+
+    } else {
+
+    }
+
+    mid = Math.floor((first + last) / 2)
+  }
+
+  return 0
 };
